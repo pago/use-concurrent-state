aa35ccdb8de0c020dedaa77d55bd87ec7bd996da
Nicolai Schulten <nicolai.schulten@trivago.com>
2021-04-21 11:39:48 +0200

    Export fromSequence to public API

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export type {OperatorApi, Operator, TaskStrategy} from './utils';
 export type {TaskEventNotifications, Task, TaskResolver} from './task';
 
-export {idleTask, task} from './task';
+export {idleTask, task, fromSequence} from './task';
 export {useConcurrentState} from './react';
 export {call, getDependencies, getState} from './operators';
